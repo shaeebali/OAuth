afe0055c030c7f3b1a10d191c5cc9e21c1ac894a
shaeeb <shaeebali@gmail.com>
2023-12-06 10:49:24 +0000

    initialize passport

--- app.js
+++ app.js
@@ -24,7 +24,15 @@ const GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET;
 /*
  * Passport Configurations
 */
-
+passport.use( new GitHubStrategy({
+  clientID: GITHUB_CLIENT_ID,
+  clientSecret: GITHUB_CLIENT_SECRET,
+  callbackURL: "http://localhost:3000/auth/github/callback" 
+  },
+  function(accessToken, refreshToken, profile, done) {
+    return done(null, profile);
+  })
+)
 
 
 
@@ -46,7 +54,7 @@ app.use(session({
   saveUninitialized: false
 }))
 
-
+app.use(passport.initialize());
 
 /*
  * Routes
